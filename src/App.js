bce7daef6f98d6566f17ef024e8e9291d17f5ef0
Rares Spatariu <sararares12@gmail.com>
2022-04-24 13:52:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { Slider } from './components/index'
+
+function App() {
+  return <div className="App">
+    <Slider/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/Slider.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import Slide from "./Slide";
+import data from "../data";
+
+const Slider = () => {
+	return <div className="container slider">
+		<Slide/>
+	</div>;
+	};
+
+export default Slider;
