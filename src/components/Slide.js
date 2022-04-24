cecc64ec55316a33bc853e48f1db328801aa51c7
Rares Spatariu <sararares12@gmail.com>
2022-04-24 14:39:50 +0200

    fine

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,6 +1,18 @@
 import React from "react";
-const Slide = () => {
-  return <div>Slide</div>;
+import starCreator from  "../utils/starCreator";
+
+const Slide = ({voto, recensione, autore,classes}) => {
+  console.log(voto)
+  return  (
+    <div className={`slide ${classes}`}>
+      <div className="review">
+        <h4>{autore}</h4>
+        <p>{recensione}</p>
+        <div className="star-container">{starCreator(voto)}</div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Slide;
